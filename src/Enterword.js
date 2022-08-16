2ae0d53f9c2c83e0b616a30579257d1c54127b3e
manashree635315 <kalodelaxmikant38@gmail.com>
2022-08-16 22:39:53 +0530

    Enterword and Match.js initiated

--- src/Enterword.js
+++ src/Enterword.js
@@ -1,10 +1,21 @@
-import { fusername } from "./Enteruser";
+import { ReactComponent as IconMenu1 } from './head.svg';
+import { ReactComponent as IconMenu2 } from './tagline.svg';
+import { Link } from "react-router-dom";
+import {fusername} from "./Enteruser";
 
 const Enterword = () => {
   console.log(fusername)
     return(
         <div>
-
+          <div>
+          <IconMenu1 className = "head"/><IconMenu2 className = "tagline"/>
+        </div>
+        <div><input placeholder={'No of letters :'}   className="textbox"/></div>
+        <Link to="/Match">
+        <button className ="button">
+            Confirm
+          </button>
+          </Link>
         </div>
     );
 }
---
+++ src/Match.js
@@ -0,0 +1,13 @@
+import { ReactComponent as IconMenu1 } from './verBnW.svg';
+
+const Match = () =>{
+    return(
+        <div>
+          <div>
+          <IconMenu1 className = "side"/>
+        </div>
+        <div>You have matched with </div> 
+        </div>
+    );
+}
+export default Match;
